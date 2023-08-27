30f0a8567266f1499b8882b758a0164cea775c61
Kashish Shukla <86675818+Kashish-Shukla18@users.noreply.github.com>
2023-08-27 21:01:45 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import React from "react"
 
 export default function App() {
     const [messages, setMessages] = React.useState(["a", "b"])
-    /**
+/**
      * Challenge:
      * - If there are no unread messages, display "You're all caught up!"
      * - If there are > 0 unread messages, display "You have <n> unread
      *   message(s)"
+     *      - If there's exactly 1 unread message, it should read "message"
+     *        (singular)
      */
     return (
         <div>
-            <p>
-            {messages.length>0 ?`You have ${messages.length} unread message(s)`:"You're all caught up!"}
-            </p>
+            {
+                messages.length === 0 ?
+                <h1>You're all caught up!</h1> :
+                <h1>You have {messages.length} unread message{messages.length > 1 && "s"}</h1>
+            }
         </div>
     )
